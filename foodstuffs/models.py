a9af7b5f7bb2dcee91caac927cdce4a5aae3b8be
Matthew Sheets <msheets@Matthews-MacBook-Pro.local>
2015-10-18 18:35:34 -0700

    updating so allergens are x-tensible <--- look at my cool tech-like spellings! woot

--- foodstuffs/models.py
+++ foodstuffs/models.py
@@ -1,13 +1,21 @@
 from django.db import models
+from sets import Set
 
 # Create your models here.
 
+class Allergen(models.Model):
+    name = models.CharField(max_length=200)
+
 class Resource(models.Model):
     name = models.CharField(max_length=200)
-    nut_free = models.BooleanField(default=false)
-    lactose_free = models.BooleanField(default=false)
-    gluten_free = models.BooleanField(default=false)
+    allergens = models.ManyToManyField(Allergen)
 
+    def has_allergen(self, allergen):
+        for a in self.allergens.all():
+            if a == allergen:
+                return true
+        return false
+    
     def __str__(self):
         return self.name
 
@@ -15,23 +23,19 @@ class Meal(models.Model):
     name = models.CharField(max_length=200)
     resources = models.ManyToManyField(Resource)
 
-    def is_nut_free(self):
+    def get_allergens(self):
+        allergens = set()
         for resource in self.resources.all():
-            if !resource.nut_free:
-                return false
-        return true
+            allergens |= set(resource.allergens)
+        return list(allergens)
 
-    def is_lactose_free(self):
+    # fastest to look through one by one rather than using get_allergens
+    # allows short-circuiting once found
+    def has_allergen(self, allergen):
         for resource in self.resources.all():
-            if !resource.lactose_free:
-                return false
-        return true
-
-    def is_gluten_free(self):
-        for resource in self.resources.all():
-            if !resource.gluten_free:
-                return false
-        return true
+            if resource.allergens.all().count(allergen) > 0:
+                return true
+        return false
 
     def __str__(self):
         return self.name
@@ -40,23 +44,19 @@ class Menu(models.Model):
     name = models.CharField(max_length=200)
     meals = models.ManyToManyField(Meal)
 
-    def is_nut_free(self):
+    def get_allergens(self):
+        allergens = set()
         for meal in self.meals.all():
-            if !meal.is_nut_free()
-                return false
-        return true
+            allergens |= set(meal.get_allergens())
+        return list(allergens)
 
-    def is_lactose_free(self):
+    # fastest to look through one by one rather than using get_allergens
+    # allows short-circuiting once found
+    def has_allergen(self, allergen):
         for meal in self.meals.all():
-            if !meal.is_lactose_free():
-                return false
-        return true
-
-    def is_gluten_free(self):
-        for meal in self.meals.all():
-            if !meal.is_gluten_free():
-                return false
-        return true
+            if meal.get_allergens().count(allergen) > 0:
+                return true
+        return false
 
     def __str__(self):
         return self.name
