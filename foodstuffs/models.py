93fb193339e9ac51a1dae0cc7f4068c0cfda4ade
George Mills <georgegmills@gmail.com>
2015-10-18 18:47:56 -0700

    poopy pants

--- foodstuffs/models.py
+++ foodstuffs/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+class Alergen(models.Model):
+    name= models.CharField(max_length=200)
+
 class Resource(models.Model):
     name = models.CharField(max_length=200)
-    nut_free = models.BooleanField(default=false)
-    lactose_free = models.BooleanField(default=false)
-    gluten_free = models.BooleanField(default=false)
-
+    alergens = models.ManyToManyField(Alergen)
+    
     def __str__(self):
         return self.name
 
@@ -15,23 +16,14 @@ class Meal(models.Model):
     name = models.CharField(max_length=200)
     resources = models.ManyToManyField(Resource)
 
-    def is_nut_free(self):
+    def get_alergens(self):
+        alergens = []
         for resource in self.resources.all():
-            if !resource.nut_free:
-                return false
-        return true
+            alergens.append(resource.alergens)
+        return 
+            
 
-    def is_lactose_free(self):
-        for resource in self.resources.all():
-            if !resource.lactose_free:
-                return false
-        return true
 
-    def is_gluten_free(self):
-        for resource in self.resources.all():
-            if !resource.gluten_free:
-                return false
-        return true
 
     def __str__(self):
         return self.name
@@ -40,23 +32,12 @@ class Menu(models.Model):
     name = models.CharField(max_length=200)
     meals = models.ManyToManyField(Meal)
 
-    def is_nut_free(self):
+    def get_meal_alergens(self):
         for meal in self.meals.all():
-            if !meal.is_nut_free()
-                return false
-        return true
 
-    def is_lactose_free(self):
-        for meal in self.meals.all():
-            if !meal.is_lactose_free():
-                return false
-        return true
+            #return all alergens associated in resource. Add them to a list? 
 
-    def is_gluten_free(self):
-        for meal in self.meals.all():
-            if !meal.is_gluten_free():
-                return false
-        return true
+
 
     def __str__(self):
         return self.name
