7c9d55e5f71827fcf5ad77e1f112b7bc704160b0
George Mills <georgegmills@gmail.com>
2015-11-22 00:32:27 -0800

    trash

--- foodstuffs/forms.py
+++ foodstuffs/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import MealResourceRelationship, Meal
+from django.forms import  inlineformset_factory
+from .models import MealResourceRelationship, Meal, Resource
 
-class MealForm(forms.ModelForm):
-    class Meta:
-        model = Meal
-        fields = '__all__'
+class MealForm(forms.Form):
+    model = Meal
+    name = forms.CharField(max_length=200)
+    resources = inlineformset_factory(Resource, Meal.resources.through, fields=('resource','units_per_person'))
+    recipe = forms.CharField(max_length=2000)
+
+   
+
